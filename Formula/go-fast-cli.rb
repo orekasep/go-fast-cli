10fd7fa7ffb8948ce82880851f250ed398e3b42f
orekasep <orekasep@users.noreply.github.com>
2026-09-21 16:18:41 +0700

    chore: update Homebrew formula to v1.1.0

--- Formula/go-fast-cli.rb
+++ Formula/go-fast-cli.rb
@@ -1,12 +1,12 @@
 class GoFastCli < Formula
   desc "Command-line tool to test internet speed using Fast.com with a live TUI"
   homepage "https://github.com/orekasep/go-fast-cli"
-  version "1.0.1"
+  version "1.1.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/orekasep/go-fast-cli/releases/download/v1.0.1/go-fast-cli_v1.0.1_darwin-arm64.tar.gz"
-      sha256 "d7f702b5abd9d3c706f29e306c7d409f74642f5655efa959156ffb54c4ac9842"
+      url "https://github.com/orekasep/go-fast-cli/releases/download/v1.1.0/go-fast-cli_v1.1.0_darwin-arm64.tar.gz"
+      sha256 "dac821854102274493945a23d4f914118d0ce6869a4c63acda4c97aab6e9c396"
 
       def install
         bin.install "fast" => "go-fast-cli"
@@ -14,8 +14,8 @@ class GoFastCli < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/orekasep/go-fast-cli/releases/download/v1.0.1/go-fast-cli_v1.0.1_darwin-amd64.tar.gz"
-      sha256 "43d0593e6ca0e1c56ad852a1e963ce5a33f9e0214bfb1cf3d4ac4166809ecaa1"
+      url "https://github.com/orekasep/go-fast-cli/releases/download/v1.1.0/go-fast-cli_v1.1.0_darwin-amd64.tar.gz"
+      sha256 "499edab0801faf74f43ab0a32682f52ca73a78a86c2d61c3ab5d225bdd976da2"
 
       def install
         bin.install "fast" => "go-fast-cli"
@@ -26,8 +26,8 @@ class GoFastCli < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/orekasep/go-fast-cli/releases/download/v1.0.1/go-fast-cli_v1.0.1_linux-arm64.tar.gz"
-      sha256 "2561db73ed6328725647500268b9e03b6a91d7341086b635b808ef13ce1e8f3a"
+      url "https://github.com/orekasep/go-fast-cli/releases/download/v1.1.0/go-fast-cli_v1.1.0_linux-arm64.tar.gz"
+      sha256 "d0523a841575b75d9ab7dcfa5299c9326896dde000559a36bbaf1879091f0f9b"
 
       def install
         bin.install "fast" => "go-fast-cli"
@@ -35,8 +35,8 @@ class GoFastCli < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/orekasep/go-fast-cli/releases/download/v1.0.1/go-fast-cli_v1.0.1_linux-amd64.tar.gz"
-      sha256 "100ed9413d5061adf0f7d8f27f3291b4e7018da36f73d7d79fd337246f76b7f9"
+      url "https://github.com/orekasep/go-fast-cli/releases/download/v1.1.0/go-fast-cli_v1.1.0_linux-amd64.tar.gz"
+      sha256 "dc4b9853a98bdcfb8eea00719ec0057891c44403563eb8050c798ca311b4aa13"
 
       def install
         bin.install "fast" => "go-fast-cli"
